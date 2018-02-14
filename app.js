ed0c4ba75b0528f3b5eb6ebff0342cf75c9d1b41
bolu ajibs <bolu.ajibawo@gmail.com>
2018-02-14 08:41:25 +0100

    setup graphql for mongodb

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const routes = require('./routes/index');
 const bodyParser = require('body-parser');
@@ -10,5 +11,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // load routes
 app.use('/', routes);
+
+module.exports = app;
+*/
+
+// <!-- End of REST -->
+
+// beginning of graphql
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
+
+/* Here a simple schema is constructed using the GraphQL schema language (buildSchema).
+   More information can be found in the GraphQL spec release */
+
+const schema = require('./src/schema');
+
+const app = express();
+app.use('/', graphqlHTTP({
+  schema,
+  graphiql: true // Set to false if you don't want graphiql enabled
+}));
 
 module.exports = app;
--- controllers/listingController.js
+++ controllers/listingController.js
@@ -1,12 +1,27 @@
-const Business = require('../models/Business');
+/* const Business = require('../models/Business');
 
 exports.getAllListings = async (req, res) => {
   const listings = await Business.find({})
     .sort({ created: 'desc' })
     .limit(6);
-
-  res.json({
-    status: 200,
-    listings
-  });
+  return listings;
+  // res.json({
+    // status: 200,
+    // listings
+  // });
 };
+*/
+
+// End of REST
+
+// beginning of GraphQL
+const Business = require('../models/Business');
+
+exports.getAllListings = async () => {
+  const listings = await Business.find({})
+    .sort({ created: 'desc' })
+    .limit(6);
+
+  return listings;
+};
+
